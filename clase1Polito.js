2f3da6144383ff4eaf54f1558ab8c7bb1144c6ac
Andrea Polito <andreaepolito@hotmail.com>
2021-09-01 22:28:11 -0300

    Tarea 1 completa
    Tarea 1 completa

---
+++ clase1Polito.js
@@ -0,0 +1,9 @@
+let nombre = prompt('Como te llamas?');
+let edad = prompt('Cuantos años tenes?');
+
+let edadANumero = parseInt(edad);
+
+
+console.log(nombre + ' en el 2030 vas a tener ' + (edadANumero + 9) + ' años.');
+
+alert(nombre + ' en el 2030 vas a tener ' + (edadANumero + 9) + ' años.');
