4a8142675d799a6b293f9d2366b42340437f0e81
Andrea Polito <andreaepolito@hotmail.com>
2021-09-02 21:10:21 -0300

    Subida desafio clase 2

--- clase1Polito.js
+++
@@ -1,9 +0,0 @@
-let nombre = prompt('Como te llamas?');
-let edad = prompt('Cuantos años tenes?');
-
-let edadANumero = parseInt(edad);
-
-
-console.log(nombre + ' en el 2030 vas a tener ' + (edadANumero + 9) + ' años.');
-
-alert(nombre + ' en el 2030 vas a tener ' + (edadANumero + 9) + ' años.');
---
+++ clase2Polito.js
@@ -0,0 +1,11 @@
+
+let edad = prompt('Cuantos años tenes?');
+
+let edadANumero = parseInt(edad);
+
+if (edadNumero >= 18){
+    console.log('Podes pasar');
+} else {
+    console.log('No podes pasar, sos menor de 18');
+}
+
