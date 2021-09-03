06eadb24753f21fa9fbc56fe588462a3d2addbb7
Andrea Polito <andreaepolito@hotmail.com>
2021-09-02 21:26:51 -0300

    Clase 2 - desafio

--- clase2Polito.js
+++ clase2Polito.js
@@ -3,7 +3,7 @@ let edad = prompt('Cuantos años tenes?');
 
 let edadANumero = parseInt(edad);
 
-if (edadNumero >= 18){
+if (edadANumero >= 18){
     console.log('Podes pasar');
 } else {
     console.log('No podes pasar, sos menor de 18');
