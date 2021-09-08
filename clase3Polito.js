1a8c8b788d33840f4e00c4c1e453dc7594ad3125
Andrea Polito <andreaepolito@hotmail.com>
2021-09-08 11:08:39 -0300

    Clase 3

--- clase3Polito.js
+++ clase3Polito.js
@@ -1,4 +1,18 @@
 
-for (let i= 0; i <=10; i++){
-    console.log('Esta es la vuelta numero: '+ i);
-}
+//calculadora de tablas hasta el 19
+
+
+let numeroTabla = prompt('Decime un numero entero');
+
+for (let i = 0; i <= 10; i++) {
+    if (numeroTabla >= 20){
+    alert('Esta tabla no la se, apreta -enter- para seguir');
+    break;
+    } else if (numeroTabla < 20){
+     console.log(numeroTabla + 'x' + i + '= '+ (numeroTabla * i));
+    } 
+    else  {
+        alert('Hola! Solo acepto numeros, enter y volve a empezar!');
+        break;
+    } 
+} 
