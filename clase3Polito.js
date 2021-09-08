8c5802cc0ee189cce662b3281b239180dae8dc6f
Andrea Polito <andreaepolito@hotmail.com>
2021-09-07 21:20:29 -0300

    Clase 3

---
+++ clase3Polito.js
@@ -0,0 +1,11 @@
+
+let edad = prompt('Cuantos años tenes?');
+
+let edadANumero = parseInt(edad);
+
+if (edadANumero >= 18){
+    console.log('Podes pasar');
+} else {
+    console.log('No podes pasar, sos menor de 18');
+}
+
