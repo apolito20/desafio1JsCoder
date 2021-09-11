8fe339364e4f78a0db64dda5f1669fe30b094cca
Andrea Polito <andreaepolito@hotmail.com>
2021-09-11 18:06:53 -0300

    Cambios

--- clase4Polito.js
+++ clase4Polito.js
@@ -1,8 +1,8 @@
-//Simulador Interactivo - Ahorros del mes
+//Desafio complementario - Ahorros del mes
 
 //variables
 let ahorrosMesAnterior = parseInt(prompt('Cuanto ahorraste el mes anterior?'));
-let sueldo = parseInt(prompt('Indica tu sueldo de este mes')) ;
+let sueldo = parseInt(prompt('Indica tu sueldo de este mes'));
 let gastosFijos = parseInt(prompt('Ingresa tu total de gastos fijos'));
 let gastosVariables = parseInt(prompt('Ingresa tu aproximado de gastos variables de este mes'));
 
@@ -10,15 +10,17 @@ let gastosVariables = parseInt(prompt('Ingresa tu aproximado de gastos variables
 // funciones
 const suma = (a, b) => a + b;
 const resta = (a, b) => a - b;
-const gananciaPlazoFijo = (ahorrosMesAnterior) => (ahorrosMesAnterior * 0.37) / 12;
+let gananciaPlazoFijo = (x) => ((x * 0.37) / 12);
 
-    
-    let positivo = suma (sueldo, suma (ahorrosMesAnterior, gananciaPlazoFijo));
-    let negativo = suma (gastosFijos, gastosVariables);
 
-    let calculoAhorroMes = resta(positivo, negativo);
+gananciaPlazoFijo(ahorrosMesAnterior);
 
-    alert(`Este mes vas a ahorrar $${calculoAhorroMes}.`);
+let plataQueIngresa = suma (sueldo, (suma (ahorrosMesAnterior, gananciaPlazoFijo)));
+let plataQueEgresa = suma (gastosFijos, gastosVariables);
+
+let calculoAhorroMes = resta(plataQueIngresa, plataQueEgresa);
+
+alert(`Este mes vas a ahorrar $${calculoAhorroMes}.`);
 
 
 
